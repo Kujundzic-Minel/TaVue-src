a7fdd1059606a1dcaf494de2df4eb808395ba5c1
Minel <Minel@example.com>
2023-10-24 14:10:10 +0200

    first commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="bg-gray-900 text-white py-8">
+        <!-- Logo -->
+        <div>
+        <img src="@/assets/img/logo_tavue_footer_blanc.png" alt="TAVUE Paris" class="w-22 h-16" />
+        </div>
+
+      <div class="container mx-auto px-4 flex justify-between items-center">
+        
+        <!-- Section : A propos -->
+        <div class="space-y-2">
+          <p class="font-bold">À propos</p>
+          <p>Qui sommes nous ?</p>
+          <p>Contacte</p>
+          <p>Mentions légales</p>
+        </div>
+  
+        <!-- Section : Nos lunettes -->
+        <div class="space-y-2">
+          <p class="font-bold">Nos lunettes</p>
+          <p>Personnaliser</p>
+        </div>
+  
+        <!-- Section : Newsletter -->
+        <div>
+          <p class="font-bold mb-4">Newsletter</p>
+          <div class="flex">
+            <button class="bg-pink-500 p-2 rounded-l-md">Mail</button>
+            <input type="email" placeholder="Mail" class="p-2 rounded-r-md text-black">
+          </div>
+        </div>
+      </div>
+  
+      <!-- Mention en bas -->
+      <p class="text-left mt-8 ml-2  text-sm text-gray-700 flex">Site réalisé dans le cadre d'une SAE à MMI Montbéliard.</p>
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+  </script>
+  
---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="bg-white p-4 shadow-md flex justify-between items-center">
+    
+    <!-- Logo -->
+    <div class="flex items-center space-x-4">
+      <img src="@/assets/img/logo_tavue.png" alt="TAVUE Paris" class="w-22 h-16" />
+    </div>
+
+    <!-- Navigation Links -->
+    <div class="flex space-x-9 items-center">
+      <a href="#" class="transition-colors duration-300 hover:text-pink-500 border-b-2 border-transparent hover:border-pink-500">Accueil</a>
+      <a href="#" class="transition-colors duration-300 hover:text-pink-500 border-b-2 border-transparent hover:border-pink-500">Lunettes</a>
+      <a href="#" class="transition-colors duration-300 hover:text-pink-500 border-b-2 border-transparent hover:border-pink-500">Personnaliser</a>
+      <a href="#" class="bg-pink-500 text-white px-4 py-2 rounded transition-colors duration-300 hover:bg-pink-600">Mon compte</a>
+    </div>
+
+    <!-- Shopping Bag Icon -->
+    <div class="ml-4">
+      <img src="@/assets/img/shopping-bag.png" alt="Shopping Bag" class="w-10 h-10" />
+    </div>
+  </div>
+
+  <div class="bg-black text-white p-4">
+    <p class="text-center flex justify-center"><span class="text-pink-500">PROFITEZ DE -30% SUR VOTRE PROCHAINE COMMANDE !</span> Temps restant : 14H54MIN45S</p>
+  </div>
+
+</template>
